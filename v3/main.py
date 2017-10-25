bfc7d994468b10c2f02c3f488c63bc7c8a35d3da
lord_santanna <adriacabeza@hotmail.com>
2017-10-25 19:54:14 +0200

    Update main.py

--- v3/main.py
+++ v3/main.py
@@ -9,8 +9,8 @@ def index():
     initial_code = file_handler.read()
     file_handler.close()
 
-    file_hanlder = open('codes/init.in', 'r')
-    initial_input = file_hanlder.read()
+    file_handler = open('codes/init.in', 'r')
+    initial_input = file_handler.read()
     file_handler.close()
 
     return render_template('index.html', init_code=initial_code, init_input=initial_input)
