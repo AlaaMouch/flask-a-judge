3079bb6fadd893ca6f90c6004d791a2d11f92b0f
lord_santanna <adriacabeza@hotmail.com>
2017-10-25 19:51:33 +0200

    Update main.py
    Just changing handler typing (to participate in the pull request's events Hacktoberfest)

--- v1/main.py
+++ v1/main.py
@@ -8,7 +8,7 @@ def index():
 	initial_code = file_handler.read()
 	file_handler.close()
 
-	file_hanlder = open('codes/init.in', 'r')
+	file_handler = open('codes/init.in', 'r')
 	initial_input = file_hanlder.read()
 	file_handler.close()
 
